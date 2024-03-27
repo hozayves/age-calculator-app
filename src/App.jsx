ac92746b9e6159295ca5ab48131bb568a6d3132e
hozayves <muhozayves2014@gmail.com>
2024-03-27 22:59:47 +0200

    fx make responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <main className='flex justify-center items-center h-screen w-full bg-OffWhite'>
-      <div className='bg-white w-[90%] h-auto rounded-3xl rounded-br-[150px] px-4 py-7'>
+      <div className='bg-white w-[90%] h-auto rounded-3xl rounded-br-[150px] px-4 py-7 sm:w-[420px] md:p-7'>
         <AgeInput />
         <AgeOutput />
       </div>
--- src/components/AgeInput.jsx
+++ src/components/AgeInput.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function AgeInput() {
   return (
     <section className='flex flex-col gap-10'>
-        <div className='flex justify-between font-poppins w-full'>
+        <div className='flex justify-between font-poppins w-full md:w-[320px]'>
             <div className='flex flex-col w-[90px] gap-1'>
                 <label htmlFor="day" className='uppercase text-SmokeyGrey font-semibold tracking-wide text-sm'>day</label>
                 <input 
-                    className='rounded-lg px-3 py-1 border border-LightGrey uppercase text-xl font-bold outline-none'
+                    className='rounded-lg px-3 py-1 border border-LightGrey uppercase text-xl font-bold outline-none md:py-2 md:text-2xl'
                     type="text"
                     placeholder='dd' 
                     id='day' />
@@ -15,7 +15,7 @@ function AgeInput() {
             <div className='flex flex-col w-[90px] gap-1'>
                 <label htmlFor="day" className='uppercase text-SmokeyGrey font-semibold tracking-wide text-sm'>month</label>
                 <input 
-                    className='rounded-lg px-3 py-1 border border-LightGrey uppercase text-xl font-bold outline-none'
+                    className='rounded-lg px-3 py-1 border border-LightGrey uppercase text-xl font-bold outline-none md:py-2 md:text-2xl'
                     type="text"
                     placeholder='mm' 
                     id='day' />
@@ -23,14 +23,14 @@ function AgeInput() {
             <div className='flex flex-col w-[90px] gap-1'>
                 <label htmlFor="day" className='uppercase text-SmokeyGrey font-semibold tracking-wide text-sm'>year</label>
                 <input 
-                    className='rounded-lg px-3 py-1 border border-LightGrey uppercase text-xl font-bold outline-none'
+                    className='rounded-lg px-3 py-1 border border-LightGrey uppercase text-xl font-bold outline-none md:py-2 md:text-2xl'
                     type="text"
                     placeholder='yyyy' 
                     id='day' />
             </div>
         </div>
-        <div className='border border-LightGrey relative mb-16'>
-            <button className='bg-purple absolute w-[50px] h-[50px] rounded-full left-[40%] -top-[25px] p-3 cursor-pointer'>
+        <div className='border border-LightGrey relative mb-16 md:mb-14'>
+            <button className='bg-purple absolute w-[50px] h-[50px] rounded-full left-[40%] -top-[25px] p-3 cursor-pointer md:left-[90%]'>
                 <img src="/icon-arrow.svg" alt="" />
             </button>
         </div>
