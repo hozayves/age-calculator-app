4dd5bfd543f8a724fa68724d8db11fd1aee8a091
hozayves <muhozayves2014@gmail.com>
2024-03-28 20:43:16 +0200

    rafactor design appearance

--- src/components/AgeInput.jsx
+++ src/components/AgeInput.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function AgeInput() {
   return (
     <section className='flex flex-col gap-10'>
--- src/components/AgeOutput.jsx
+++ src/components/AgeOutput.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 function AgeOutput() {
   return (
-    <section className='flex flex-col font-poppins font-bold text-5xl italic gap-2'>
-        <article><span className='text-purple'>38</span> years</article>
-        <article><span className='text-purple'>3</span> months</article>
-        <article><span className='text-purple'>3</span> days</article>
+    <section className='flex flex-col font-poppins font-extrabold text-5xl italic gap-2'>
+        <article className="flex gap-[8px] items-end"><span className='text-purple text-6xl'>38</span>years</article>
+        <article className="flex gap-[8px] items-end"><span className='text-purple text-6xl'>3</span>months</article>
+        <article className="flex gap-[8px] items-end"><span className='text-purple text-6xl'>26</span>days</article>
     </section>
   )
 }
